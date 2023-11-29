aa2c7337a9b2613723d6c4c89f07b2b36271ee20
zf26 <2632481786@qq.com>
2023-11-29 20:15:20 +0800

    .

--- mps-ai-education/src/components/HomePage.vue
+++ mps-ai-education/src/components/HomePage.vue
@@ -1,19 +1,34 @@
 <template>
   <div>
     <img src="../assets/images/logo.png" alt="" class="topimg">
+    <div style="display: inline-block;width: 55%;"></div>
     <top-bar></top-bar>
+    <div class="bodybox">
+         <div class="mainbox">
+
+         </div>
+    </div>
   </div>
 </template>
 
 <script>
-import TopBar from '../view/TopBar.vue'
+import TopBar from '@/view/TopBar.vue'
 export default {
-  components: { TopBar },
+  components:{TopBar},
+    data(){
+     return{
+        
+     } 
+    }
 
 }
 </script>
 
 <style>
+body{
+  margin: 0px;
+  padding: 0px;
+}
 .topimg{
   width: 50%;
   position: fixed;
@@ -21,5 +36,16 @@ export default {
   left: -150px;
   opacity: 0.8;
   transform: scale(0.6);
+  z-index: -1;
+}
+.bodybox{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.mainbox{
+  background-color: antiquewhite;
+  width: 80%;
+  height: 500px;
 }
 </style>
--- mps-ai-education/src/view/TopBar.vue
+++ mps-ai-education/src/view/TopBar.vue
@@ -2,28 +2,42 @@
     <div class="topNav">
         <div class="Head_Navigation">
         <div>首页</div>
-        <div class="Way">人工智能
+        <div class="Way">编程语言
             <ul class="Down_Menu">
-            <li>人工智能</li>
-            <li>人工智能</li>
-            <li>人工智能</li>   
+            <li>Python基础</li>
+            <li>Python进阶</li>
+            <li>工具包</li>   
            </ul></div>
+        <div>体验AI</div>
         <div>人工智能</div>
-        <div>人工智能</div>
-        <div class="Way">人工智能
+        <div class="Way">我的
             <ul class="Down_Menu">
-                <li>人工智能</li>
-                <li>人工智能</li>
-                <li>人工智能</li>   
+                <li>基本信息</li>
+                <li>学习课程</li>
+                <li @click="ExitLogin">退出登录</li>   
                </ul>
         </div>
+    </div>
+    <div class="baseinFor">
+        <img src="../assets/images/屏幕截图 2023-10-28 111613.png" class="hadpro" style="width: 50px;height: 50px;border-radius: 50%;
+        position: absolute;left:0px;transition: all .5s;">
+        <span class="usename">啊巴啊巴</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+     return{
+        
+     } 
+    },
+    methods:{
+        ExitLogin(){
+            this.$router.push('/loginfrom')
+        }
+    }
 }
 </script>
 
@@ -31,12 +45,12 @@ export default {
 .Head_Navigation{
     position: relative;
     display: flex;
-    margin-top: 80px;
     height: 50px;
     width: 100%;
     background-color:rgba(120, 247, 247, 0.6);
     justify-content: center;
-    z-index: 2;
+    z-index: 1;
+    cursor: pointer;
 }
 .Head_Navigation div{
     font-size: 16px;
@@ -50,7 +64,12 @@ export default {
 }
 .Head_Navigation div:nth-child(2):hover,.Head_Navigation div:nth-child(5):hover{
     background-color: #79cffa;
+    border-radius: 5px;
     height: 200px;
+}
+.Head_Navigation :hover{
+    background-color: #79cffa;
+    border-radius: 5px;
 }
 .Down_Menu{
     margin: 0px;
@@ -59,13 +78,44 @@ export default {
     background-color:#79cffa;
     transition: all .5s; 
 }
-.Way:hover .Down_Menu{
-    display: block;
-}
 .Down_Menu li{
     width: 200px;
 }
 .Down_Menu li:hover{
     background-color: rgba(120, 247, 247, 0.6);
+}
+.baseinFor{
+  z-index: 2;
+  width: 50px;
+  height: 50px;
+  display: inline-block;
+  border-radius: 50px;
+  position: absolute;
+  top: 78px;
+  left: 85%;
+  overflow: hidden;
+  transition: all .5s;
+  background: rgb(255, 255, 255, 0.2);
+  box-shadow: 0 25px 45px rgb(0, 0, 0, 0.1);
+  border: 1px solid rgb(255, 255, 255, 0.8);
+}
+.hadpro:hover{
+  box-shadow: 0px 0px 150px 100px rgba(11, 252, 252, 0.5);
+  border:1px pink solid;
+  transform: scale(1.2) translateX(10px);
+}
+.baseinFor:hover{
+  width: 200px;
+}
+.usename{
+  position: absolute;
+  font-weight: 700;
+  top: 30%;
+  right: 30%;
+  opacity: 0;
+  transition: all .5s;
+}
+.baseinFor:hover .usename{
+  opacity: 1;
 }
 </style>
